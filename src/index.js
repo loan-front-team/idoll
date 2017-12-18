d0d7a97f9936827eb9b710195c2168bc418ff308
lulu27753 <lulu27753@163.com>
2017-12-18 15:37:52 +0800

    +icon

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import { Layout, Sider, Header, Content, Footer } from '../components/layout'
-import Button from '../components/button/button.jsx'
+// import Button from '../components/button/button.jsx'
+import Icon from '../components/icon/index.jsx'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <Button className='btn' type='primary' size='small'>按钮</Button>,
+  <Icon type='home' />,
   document.getElementById('root'));
 registerServiceWorker();
