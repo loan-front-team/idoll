18501a9d1b0f73c64ff69490dfba91e42a9e1d50
lulu27753 <lulu27753@163.com>
2017-12-25 10:02:44 +0800

    +collapse

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Home from './Home'
+import Collapse from './templates/collapse'
 
 import './App.css';
 
@@ -9,6 +10,7 @@ const App = () => (
   <Router>
     <div>
       <Route exact path='/' component={Home} />
+      <Route exact path='/collapse' component={Collapse} />
     </div>
   </Router>
   )
