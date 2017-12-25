b996d959007642cb1c1e32c20e54c011cc33df2a
lulu27753 <lulu27753@163.com>
2017-12-25 10:02:44 +0800

    +collapse

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
-import App from './App.js'
+import App from './App.jsx'
 
 import './index.css';
 
