fc6bf2087640d2ae6cbba65e406a611bcb3c83c3
zlh <18861544875@163.com>
2017-12-29 11:36:33 +0800

    add 动态进度条

--- src/templates/progress.jsx
+++ src/templates/progress.jsx
@@ -2,6 +2,24 @@ import React from 'react';
 import Progress from '../../components/progress';
 
 export default class ProgressView extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      percentValue: 0
+    }
+  }
+  componentDidMount() {
+    let interval = setInterval(() => {
+      let percentValue = this.state.percentValue + 1;
+      if (percentValue < 100) {
+        this.setState({percentValue: percentValue});
+      } else {
+        percentValue = 100;
+        this.setState({percentValue: percentValue});
+        clearInterval(interval)
+      }
+    }, 100)
+  }
   render() {
     return (
       <div id='main-container'>
@@ -15,6 +33,7 @@ export default class ProgressView extends React.Component {
         <Progress type='circle' percent={50} status='active' gapPosition='bottom' />
         <Progress type='circle' percent={70} status='exception' gapPosition='bottom' />
         <Progress type='circle' percent={100} gapPosition='left' />
+        <Progress type='circle' percent={this.state.percentValue} gapPosition='left' />
         <h1 className='h1'>仪表盘型进度条</h1>
         <Progress type='dashboard' percent={100} status='active' gapPosition='top' />
         <Progress type='dashboard' percent={100} status='exception' gapPosition='bottom' />
