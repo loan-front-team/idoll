aaddb3b02789a95a9d5120744d1bb3249354671d
lulu27753 <lulu27753@163.com>
2018-01-02 09:59:33 +0800

    @bugfix onClick

--- components/button/button.jsx
+++ components/button/button.jsx
@@ -7,6 +7,7 @@ import './style/index.js'
 export default class Button extends React.Component {
 	static defaultProps = {
 	    prefixCls: 'idoll-btn',
+	    onClick() {},
 	    ghost: false,
 	    loading: false
   	}
