25e022e4c725af11b22383a76d1b889ff14fb442
yangyang <575001446@qq.com>
2018-01-08 18:20:16 +0800

    提交modal基本目录文件

---
+++ components/modal/Modal.jsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+// https://www.npmjs.com/package/rc-dialog
+import Dialog from 'rc-dialog';
+import PropTypes from 'prop-types';
+// https://www.npmjs.com/package/rc-util
+import addEventListener from 'rc-util/lib/Dom/addEventListener';
+import Button from '../button';
+import { getConfirmLocale } from './locale';
