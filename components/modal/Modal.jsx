1231edebb20c340ab2bb77110dca071326ece91f
yangyang <575001446@qq.com>
2018-01-09 15:43:52 +0800

    删除原有modal组件文件夹

--- components/modal/Modal.jsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-// https://www.npmjs.com/package/rc-dialog
-import Dialog from 'rc-dialog';
-import PropTypes from 'prop-types';
-// https://www.npmjs.com/package/rc-util
-import addEventListener from 'rc-util/lib/Dom/addEventListener';
-import Button from '../button';
-import { getConfirmLocale } from './locale';
