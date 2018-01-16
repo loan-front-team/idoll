353ce7370476a013228d1d34d9f6a1a3804aff47
lulu27753 <lulu27753@163.com>
2018-01-16 17:55:07 +0800

    +img-responsive

--- src/HomeIndex/Page3.jsx
+++ src/HomeIndex/Page3.jsx
@@ -12,15 +12,15 @@ export class Page3 extends Component {
     <div className='image-wrapper'>
       <div>
         <span className='h3'>用研+设计</span>
-        <img src='../../assets/images/row1.png' alt='用研+设计' />
+        <img src='../../assets/images/row1.png' alt='用研+设计' className='row1' />
       </div>
       <div>
         <span className='h3'>包装+实现</span>
-        <img src='../../assets/images/row2.png' alt='包装+实现' />
+        <img src='../../assets/images/row2.png' alt='包装+实现' className='row2' />
       </div>
       <div>
         <span className='h3'>管理+创新</span>
-        <img src='../../assets/images/row3.png' alt='管理+创新' />
+        <img src='../../assets/images/row3.png' alt='管理+创新' className='row3' />
       </div>
     </div>
   </div>
