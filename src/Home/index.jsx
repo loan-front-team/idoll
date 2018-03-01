107bf217b1c9096f4bd24cef14d1930283e956d4
yangyang <575001446@qq.com>
2018-03-01 09:39:33 +0800

    提交spin组件

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -127,6 +127,12 @@ function Home() {
           <div className='info'>auto-complete</div>
         </NavLink>
       </div>
+      <div className='widget-item'>
+        <NavLink className='widget-wrap' to='/spin'>
+          <div className='name'>加载中</div>
+          <div className='info'>spin</div>
+        </NavLink>
+      </div>
     </div>
   );
 }
