4e2ba6581684a638ddc6469e9690ba1d13c3efe1
roamer123 <604070939@qq.com>
2018-03-21 18:45:27 +0800

    @bug input select

--- src/templates/select.jsx
+++ src/templates/select.jsx
@@ -8,21 +8,21 @@ const select = () => (
   <div id='main-container'>
     <h1 className='h1'>普通下拉框</h1>
     <Select defaultValue='jane' style={{ width: 120, margin: 30 }}>
-      <Option value='jane'>jane</Option>
-      <Option value='mick'>mick</Option>
+      <Option value='jane'>大明</Option>
+      <Option value='mick'>大大</Option>
     </Select>
     <br />
     <h1 className='h1'>搜索下拉框</h1>
-    <Select showSearch style={{ width: 160, margin: 30 }} placeholder='Select a person' >
-      <Option value='jane'>jane</Option>
-      <Option value='mick' disabled >mick</Option>
+    <Select showSearch style={{ width: 160, margin: 30 }} placeholder='请选择' >
+      <Option value='jane'>小小</Option>
+      <Option value='mick' disabled >大明</Option>
     </Select>
     <br />
     <h1 className='h1'>多选下拉框</h1>
-    <Select mode='multiple' style={{ width: 160, margin: 30 }} placeholder='Select a person' >
-      <Option value='jane'>jane</Option>
-      <Option value='jack'>jack</Option>
-      <Option value='mick' >mick</Option>
+    <Select mode='multiple' style={{ width: 160, margin: 30 }} placeholder='请选择' >
+      <Option value='jane'>小明</Option>
+      <Option value='jack'>大明</Option>
+      <Option value='mick' >大大明</Option>
     </Select>
   </div>
 )
